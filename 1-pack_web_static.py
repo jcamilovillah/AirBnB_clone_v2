cb4b89fe06a5873da318fab3d39ca32f50ececf4
jcamilovillah <villahjuancamilo@gmail.com>
2020-12-23 02:10:01 +0000

    .tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Fabric script that generates a .tgz archive
+""" Pack web static """
+from fabric.api import local
+import datetime import datetime
+
+
+def do_pack():
+    """ Fabric script that generates a .tgz archive """
+    local("mkdir -p versions")
+    format_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    new_file = local(
+        "tar -cvzf versions/web_static_{}.tgz web_static".format(format_time))
+    if new_file.succeeded:
+        return "versions/web_static_{}.tgz web_static".format(format_time)
+    else:
+        return None
